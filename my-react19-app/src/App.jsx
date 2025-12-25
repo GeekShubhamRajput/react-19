dbbcfce35b9eaa7b7abae953859e353755c0bf5c
Shubham Rajput <rajput.shubh1993@gmail.com>
2025-12-25 23:01:24 +0530

    Rendering List

--- my-react19-app/src/App.jsx
+++ my-react19-app/src/App.jsx
@@ -9,15 +9,12 @@ import UserCard from './components/UserCard.jsx'
 import CardWrapper from './components/CardWrapper.jsx'
 import { Userdetails } from './components/UserDetails.jsx'
 import { UserDetail } from './components/Userdetail.jsx'
+import { ProductList } from './components/ProductList.jsx'
 
 function App() {
   return (
     <>
-      <Userdetails name="James" isOnline={true} />
-      <Userdetails name="Clark" isOnline={false} />
-      <hr />
-      <UserDetail name="Evan" isOnline={true} isPremium={true} isNewUser={false} role='admin'/>
-      <UserDetail name="Ben" isOnline={false} isPremium={false} isNewUser={true} role='vip'/>
+      <ProductList />
     </>
   )
 }
---
+++ my-react19-app/src/components/ProductList.jsx
@@ -0,0 +1,23 @@
+export const ProductList = () => {
+  const products = [
+    {id: 1, name: 'Laptop', price: 999},
+    {id: 2, name: 'Mobile', price: 666},
+    {id: 3, name: 'Charger', price: 99}
+  ]
+
+  const ProductDetail = products.map((product) => {
+          return(
+            <div>
+              <h3>{product.name}</h3>
+              <p>Price: ${product.price}</p>
+            </div>
+          )
+        })
+
+  return(
+    <div>
+      <h2>Our Products</h2>
+      {ProductDetail}
+    </div>
+  )    
+}
